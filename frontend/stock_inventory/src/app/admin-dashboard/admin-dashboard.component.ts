59cbda7ecb50cefc16d4486615c112a2e085f09b
erhic <eric.gichovi@student.moringaschool.com>
2022-03-31 16:50:14 +0300

    feature: admin-dashboard- added the bar graph templates and it logic and styling

--- frontend/stock_inventory/src/app/admin-dashboard/admin-dashboard.component.ts
+++ frontend/stock_inventory/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -10,17 +10,7 @@ export class AdminDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-
-// linear graph
-
-
-
-
-
-
-// bar graph
-
+const ct = document.getElementById('mChart');
 const ctx = document.getElementById('myChart');
 const myChart = new Chart('myChart', {
     type: 'line',
@@ -56,5 +46,40 @@ const myChart = new Chart('myChart', {
       }
   }
 });
+
+  
+  const myChat = new Chart('mChart', {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          // y: {
+          //     beginAtZero: true
+          // }
+      }
   }
-}
+});
+  }}
--- frontend/stock_inventory/src/app/app.module.ts
+++ frontend/stock_inventory/src/app/app.module.ts
@@ -3,14 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import {CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDividerModule} from '@angular/material/divider';
+// import {AppRoutingModule} from '../a';;
 @NgModule({
   declarations: [
     AppComponent,
     AdminDashboardComponent,
   ],
   imports: [
-    BrowserModule
+   
+    BrowserModule,
+    BrowserAnimationsModule,
+
+
+   
+    // AppRoutingModule,
+    MatDividerModule,
+
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
