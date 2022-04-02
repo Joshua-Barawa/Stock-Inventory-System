b14a097622832a25b92dc3d7ad812e488461c86a
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-04-02 17:05:34 +0300

    added clerks features

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('add/', add_product, name='add-product'),
     path('status/paid/', paid_products, name='paid-products'),
     path('status/unpaid/', unpaid_products, name='unpaid-products'),
-    path('status/change/<str:id>', change_payment_status, name='change-payment-status'),
+    path('status/change/<str:id>/', change_payment_status, name='change-payment-status'),
     path('request/', make_request, name='request-product'),
-    path('request/approve/<str:id>', approve_request, name='approve-request'),
-    path('request/decline/<str:id>', decline_request, name='decline-request'),
+    path('request/approve/<str:id>/', approve_request, name='approve-request'),
+    path('request/decline/<str:id>/', decline_request, name='decline-request'),
 ]
