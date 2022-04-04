fa3d9301bc0989ec00272bba99d8b4653d9dc598
amon <amon@amon.amon>
2022-04-04 07:57:00 +0300

    Add the name of the response from API

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,2 +1,8 @@
 export class Item {
+    items_received!: number;
+    items_in_stock!: number;
+    payment_status!: Text;
+    items_spoiled!: number;
+    buying_price!: number;
+    selling_price!: number;
 }
