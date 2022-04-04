fd3979ffe76e95474e4e6915a1307c4a96b1a76f
amon <amon@amon.amon>
2022-04-04 07:10:24 +0300

    Create a get request method to fetch data from API

--- src/app/clerk.service.ts
+++ src/app/clerk.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClerkService {
+  public baseUrl = "https://stock-inv.herokuapp.com/v1/product/add/"
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  // Create a get request method to fetch data from API
+  public getClerk(): Observable<any> {
+    return this.httpClient.get(this.baseUrl);
+  }
 }
