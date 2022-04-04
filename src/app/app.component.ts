1f3afad856647801b3fff4c8b7b7cd217db5fe4c
amon <amon@amon.amon>
2022-04-04 08:25:39 +0300

    Modify app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ClerkService } from './clerk.service';
+import { Item } from './item';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  items!: Item[];
+  item: Item = new Item();
+  constructor(private clerkService: ClerkService, private route: Router,) {}
+
+  ngOnInit(): void {
+    this.clerkService.getClerk().subscribe(data => {
+      console.log(data);
+      this.items = data;
+    })
+    throw new Error('Method not implemented.');
+  }
   title = 'clerk';
+
+  itemDetails(id: number) {
+    this.route.navigate(['items', id]);
+    console.log(id);
+  }
 }
