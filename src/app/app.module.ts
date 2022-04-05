f8fba003a009e2c4c10fdcab06a8e7175e1c2c83
amon <amon@amon.amon>
2022-04-05 08:38:22 +0300

    Add Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,25 @@ import { ClerkComponent } from './clerk/clerk.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
+
+import {CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDividerModule} from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import {AppRoutingModule,routingComponents} from './app.routing.module'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,6 +37,23 @@ import { HttpClientModule } from '@angular/common/http';
     
     // import HttpClientModule after BrowserModule.
     HttpClientModule,
+
+    MatDividerModule,
+
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
