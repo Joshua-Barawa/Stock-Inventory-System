d96a9d2760eef6b9859219ef1d7b8626253d63b3
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-04-12 20:36:43 +0300

    Added the prder-request component

--- stock-app/src/app/app-routing.module.ts
+++ stock-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent} from './dashboard/dashboard.component';
+import { OrderRequestComponent} from './order-request/order-request.component';
 
 const routes: Routes = [
    {path:'', redirectTo:'login', pathMatch:'full'},
    {path:'login', component:LoginComponent},
    {path:'signup', component:SignupComponent},
-   {path:'dashboard', component:DashboardComponent}
+   {path:'dashboard', component:DashboardComponent},
+   {path:'order-requests', component:OrderRequestComponent}
 
 ];
 
--- stock-app/src/app/app.module.ts
+++ stock-app/src/app/app.module.ts
@@ -11,7 +11,25 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import {CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDividerModule} from '@angular/material/divider';
+import { OrderRequestComponent } from './order-request/order-request.component';
+
+// import {AppRoutingModule} from '../a';;
 
 @NgModule({
   declarations: [
@@ -19,6 +37,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     LoginComponent,
     SignupComponent,
     DashboardComponent,
+    NavbarComponent,
+    OrderRequestComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +47,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ReactiveFormsModule,
     HttpClientModule,
     
+     // AppRoutingModule,
+     MatDividerModule,
+     CommonModule,
+     MatTabsModule,
+     MatSidenavModule,
+     MatToolbarModule,
+     MatIconModule,
+     MatButtonModule,
+     MatListModule,
+     MatMenuModule,
+     MatTableModule,
+     MatSortModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatPaginatorModule
     
    
     
