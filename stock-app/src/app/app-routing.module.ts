4a71c41020d400eaaf4f48c02d7d0ec892da0225
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-04-13 01:42:08 +0300

    add routing for components

--- stock-app/src/app/app-routing.module.ts
+++ stock-app/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent} from './dashboard/dashboard.component';
 import { OrderRequestComponent} from './order-request/order-request.component';
+import { RequestsComponent } from './requests/requests.component'
+import { ItemsComponent } from './items/items.component'
 
 const routes: Routes = [
    {path:'', redirectTo:'login', pathMatch:'full'},
    {path:'login', component:LoginComponent},
    {path:'signup', component:SignupComponent},
    {path:'dashboard', component:DashboardComponent},
-   {path:'order-requests', component:OrderRequestComponent}
+   {path:'order-requests', component:OrderRequestComponent},
+   {path:'requests', component:RequestsComponent},
+   {path:'items', component:ItemsComponent}
 
 ];
 
